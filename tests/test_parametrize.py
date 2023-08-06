d69b5b2b58a891084fa466e5d780e2c7f3cc9842
Svetlana Popova <pss96.svetlana@gmail.com>
2023-08-07 00:29:43 +0300

    initial commit

---
+++ tests/test_parametrize.py
@@ -0,0 +1,39 @@
+"""
+Переопределите параметр с помощью indirect параметризации на уровне теста
+"""
+import pytest
+from selene import browser, have
+
+mobile_only = pytest.mark.parametrize('browser_manager', [(375, 667), (414, 896), (390, 844)], indirect=True)
+desktop_only = pytest.mark.parametrize('browser_manager', [(1366, 768), (1600, 900), (1920, 1080)], indirect=True)
+
+
+@pytest.fixture(params=[(1366, 768), (1600, 900), (1920, 1080),
+                        (375, 667), (414, 896), (390, 844)],
+                scope='function',
+                autouse=True)
+def browser_manager(request):
+    browser.config.base_url = 'https://github.com'
+    browser.config.window_width = request.param[0]
+    browser.config.window_height = request.param[1]
+    yield
+    browser.quit()
+
+
+@desktop_only
+def test_github_desktop(browser_manager):
+    browser.open('/')
+
+    browser.element('[class~="HeaderMenu-link--sign-in"]').click()
+
+    browser.element('[class="auth-form-header p-0"]').should(have.text('Sign in to GitHub'))
+
+
+@mobile_only
+def test_github_mobile(browser_manager):
+    browser.open('/')
+
+    browser.element('[class="flex-1 flex-order-2 text-right"] .Button-content').click()
+    browser.element('[class~="HeaderMenu-link--sign-in"]').click()
+
+    browser.element('[class="auth-form-header p-0"]').should(have.text('Sign in to GitHub'))
